7e78682396a468914277620b587c2c27119c35d5
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-11-30 20:07:28 -0300

    Filtros pdf

--- app/Http/Controllers/PetsReportController.php
+++ app/Http/Controllers/PetsReportController.php
@@ -8,11 +8,29 @@ use Illuminate\Http\Request;
 
 class PetsReportController extends Controller
 {
-    public function export() {
-        $pets = Pet::all();
+    public function export(Request $request) {
+        $pets = Pet::query();
+        $filters = $request->query();
+        if ($request->has('name') && !empty($filters['name'])) {
+            $pets->where('name', 'ilike', '%' . $filters['name'] . '%');
+        }
+
+        if ($request->has('age') && !empty($filters['age'])) {
+            $pets->where('age', $filters['age']);
+        }
+
+        if ($request->has('size') && !empty($filters['size'])) {
+            $pets->where('size', $filters['size']);
+        }
+
+        if ($request->has('weight') && !empty($filters['weight'])) {
+            $pets->where('weight', $filters['weight']);
+        }
+
+        $result = $pets->get();
 
         $pdf = Pdf::loadView('pdfs.pets', [
-            'pets' => $pets
+            'pets' => $result
         ]);
 
         return $pdf->stream('relatorio_pets.pdf');
