f0c53360772963e1be2b26384a8c9b7be9440006
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-01-31 21:22:56 -0300

    Torna rotas publicas

--- routes/api.php
+++ routes/api.php
@@ -9,26 +9,28 @@ use App\Http\Controllers\RaceController;
 use App\Http\Controllers\SpecieController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VaccineController;
+use App\Http\Middleware\ValidateLimitStudentsToUser;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
 
     //Route::post('races', [RaceController::class, 'store'])->middleware(['ability:create-races']);
-    //Route::get('races', [RaceController::class, 'index'])->middleware(['ability:get-races']);
+    Route::get('races', [RaceController::class, 'index'])->middleware(['ability:get-races']);
 
     // BODY -> cadastrar, atualizar
     // Query paraments -> GET -> listar dados
     // Route params -> DELETE, PUT, GET(unico)->middleware(['auth:sanctum']);
 
     Route::post('species', [SpecieController::class, 'store'])->middleware(['ability:create-species']);
-  //  Route::get('species', [SpecieController::class, 'index'])->middleware(['ability:get-species']);
+
     Route::delete('species/{id}', [SpecieController::class, 'destroy'])->middleware(['ability:delete-species']);
 
-  //  Route::get('pets', [PetController::class, 'index'])->middleware(['ability:get-pets']);
-  //  Route::post('pets', [PetController::class, 'store'])->middleware(['ability:create-pets']);
+    //Route::get('pets', [PetController::class, 'index'])->middleware(['ability:get-pets']);
+    //Route::post('pets', [PetController::class, 'store'])->middleware(['ability:create-pets', ValidateLimitStudentsToUser::class]);
     Route::delete('pets/{id}', [PetController::class, 'destroy'])->middleware(['ability:delete-pets']);
 
     Route::get('pets/export', [PetsReportController::class, 'export'])->middleware(['ability:export-pdf-pets']);
+
 
     Route::post('clients', [ClientController::class, 'store'])->middleware(['ability:create-clients']);
     Route::get('clients', [ClientController::class, 'index'])->middleware(['ability:get-clients']);
@@ -39,12 +41,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('vaccines', [VaccineController::class, 'store'])->middleware(['ability:create-vaccines']);
 
     Route::post('logout', [AuthController::class, 'logout']);
+
+
+    Route::get('pets/perfil', [PetsReportController::class, 'showPerfil']);
 });
-Route::get('races', [RaceController::class, 'index']);
+
 Route::post('races', [RaceController::class, 'store']);
-Route::post('pets', [PetController::class, 'store']);
-Route::get('pets', [PetController::class, 'index']);
+Route::get('races', [RaceController::class, 'index']);
 Route::get('species', [SpecieController::class, 'index']);
+Route::get('pets', [PetController::class, 'index']);
+Route::post('pets', [PetController::class, 'store']);
 
 Route::post('login', [AuthController::class, 'store']);
 Route::post('users', [UserController::class, 'store']);
