dbab01e809704f986c6d891235bf1267d0fa63b7
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-12 17:13:29 -0300

    Adiciona rotas do controle de adoção

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdoptionController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\PetController;
@@ -41,3 +42,7 @@ Route::post('logout', [AuthController::class, 'logout']);
 });
 Route::post('users', [UserController::class, 'store']);
 Route::post('login', [AuthController::class, 'store']);
+
+
+Route::get('pets/adocao', [AdoptionController::class, 'index']);
+Route::get('pets/{id}', [AdoptionController::class, 'show']);
