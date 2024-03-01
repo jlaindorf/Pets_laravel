954106d28114008f7cc459b4829803afaf7be787
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-03-01 16:23:13 -0300

    Feat User test concluido

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -43,6 +43,7 @@ class UserTest extends TestCase
     public function test_user_admin_permissions_load_correct()
     {
 
+
         $response = $this->post('/api/login', [
             'email' => env("DEFAULT_EMAIL"),
             'password' => env("DEFAULT_PASSWORD")
@@ -70,13 +71,101 @@ class UserTest extends TestCase
             ]
         ]);
     }
-  /*  public function test_logout()
+
+    public function test_user_veterinario_permissions_load_correct()
+    {
+
+
+        $user = User::factory()->create(['profile_id' => 3, 'password' => '12345678']);
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => '12345678'
+        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJson([
+            'data' => [
+                'permissions' => [
+                    'create-pets',
+                    'get-pets',
+                    'delete-pets',
+                    'export-pdf-pets',
+                    'create-clients',
+                    'get-clients',
+                    'get-species',
+                    'get-races'
+                ]
+            ]
+        ]);
+    }
+
+    /* pesquisar validar informacoes */
+    public function test_user_recepcionista_permissions_load_correct()
+    {
+
+        $user = User::factory()->create(['profile_id' => 3, 'password' => '12345678']);
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => '12345678'
+        ]);
+
+        $response->assertStatus(201);
+
+
+        $response->assertJson([
+            'data' => [
+                'permissions' => [
+                    'create-pets',
+                    'get-pets',
+                    'delete-pets',
+                    'export-pdf-pets',
+                    'create-clients',
+                    'get-clients',
+                    'get-species',
+                    'get-races'
+                ]
+            ]
+        ]);
+    }
+
+    public function test_check_bad_request_login_api_response(): void
+    {
+
+
+        $response = $this->post('/api/login', [
+            'email' => "juca@hotmail.com",
+            'password' => "8712541"
+        ]);
+
+        // Verificar se o status code está como esperado
+        $response->assertStatus(401);
+
+        $response->assertJson([
+            "message" => "Não autorizado. Credenciais incorretas",
+            "status" => 401,
+            "errors" => [],
+            "data" => []
+        ]);
+    }
+
+    /*
+
+    VALIDAR LOGOUT
+
+    public function test_make_logout_in_application(): void
     {
         $this->seed(Profiles::class);
         $this->seed(InitialUser::class);
-        $user = User::factory()->create(['profile_id'=>1]);
+
+        $user = User::factory()->create(['profile_id' => 1, ]);
+
         $response = $this->actingAs($user)->post('/api/logout');
 
         $response->assertStatus(204);
-    }*/
+
+    }
+    */
 }
