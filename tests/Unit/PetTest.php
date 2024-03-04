b7e2e846e1dc74995613527de39140d5c0367d30
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-03-04 15:27:19 -0300

    Feat. Adiciona resposta no teste do metodo de atualizacao de pet

--- tests/Unit/PetTest.php
+++ tests/Unit/PetTest.php
@@ -24,6 +24,21 @@ class PetTest extends TestCase
 
         $user = User::factory()->create(['profile_id'=>2, 'password'=>'12345678']);
         $response = $this->actingAs($user)->put("/api/pets/$pet->id",$body);
-        $response->assertStatus(200);
+        $this->assertDatabaseHas('pets', [
+            'id' => $pet->id,
+            'size' => $body['size'],
+            'weight' => $body['weight']
+        ]);
+
+        $response->assertStatus(200)->assertJson([
+            'id' => true,
+            'name' => true,
+            'weight' => $body['weight'],
+            'size' => $body['size'],
+            'age' => true,
+            'race_id' => true,
+            'specie_id' => true,
+            'client_id' => null
+        ]);
     }
 }
