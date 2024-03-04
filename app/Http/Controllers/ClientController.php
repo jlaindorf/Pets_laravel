914d16c1f4e5d098a02081d7ccccaf95dceace25
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-03-04 15:16:13 -0300

    Feat. Adiciona teste de adoção de cliente

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -22,40 +22,49 @@ class ClientController extends Controller
             $request->validate([
                 'name' => 'string|required|max:255',
                 'contact' => 'string|required|max:30',
-                'email' => 'string|required|unique:people',
-                'cpf' => 'string|required|max:30|unique:people'
+                'email' => 'string|required|unique:peoples',
+                'cpf' => 'string|required|max:30|unique:peoples'
             ]);
 
             $people = People::create($data);
 
-            $client = Client::create([
+            Client::create([
                 'people_id' => $people->id
             ]);
-
 
             return $people;
         } catch (\Exception $exception) {
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
+
+
+
     public function index(Request $request)
     {
 
         $search = $request->input('name');
 
         $clients = Client::query()
-        ->select('id as client_id', 'bonus', 'people_id')
-        ->with('people')
-        ->whereHas('people', function ($query) use ($search) {
-            $query
-                // ->select('id','name','cpf', 'email', 'contact')
-                ->where('name', 'ilike', "%$search%")
-                ->orWhere('cpf', 'ilike', "%$search%")
-                ->orWhere('contact', 'ilike', "%$search%")
-                ->orWhere('email', 'ilike', "%$search%");
-        })
-        ->get();
+            ->select('id as client_id', 'bonus', 'people_id')
+            ->with('people')
+           /* ->with(
+                [
+                    'people',
+                    fn ($query) => $query->select('id', 'name', 'cpf', 'email', 'contact')
+                ]
+            )
+            */
+            ->whereHas('people', function ($query) use ($search) {
+                $query
+                    // ->select('id','name','cpf', 'email', 'contact')
+                    ->where('name', 'ilike', "%$search%")
+                    ->orWhere('cpf', 'ilike', "%$search%")
+                    ->orWhere('contact', 'ilike', "%$search%")
+                    ->orWhere('email', 'ilike', "%$search%");
+            })
+            ->get();
 
-    return $clients;
+        return $clients;
     }
 }
